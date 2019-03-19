5fa2d8e465f0e3987a18d3b48c1d5074343fd81a
ShannonTully <scatman.st@gmail.com>
2019-03-18 17:17:25 -0700

    Add the program?

---
+++ Prework-Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Prework_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add");
+            Console.Write("First number: ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Add(num1, num2).ToString());
+
+            Console.WriteLine("Subtract");
+            Console.Write("First number: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Subtract(num1, num2).ToString());
+
+            Console.WriteLine("Multiply");
+            Console.Write("First number: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Multiply(num1, num2).ToString());
+
+            Console.WriteLine("Divide");
+            Console.Write("First number: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Divide(num1, num2).ToString());
+
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static float Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
